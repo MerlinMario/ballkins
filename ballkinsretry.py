b2f614f3ac25a38d083dc30e5c562d8891957777
MerlinMario <86819056+MerlinMario@users.noreply.github.com>
2021-08-20 13:34:12 +0530

    altered

--- ballkinsretry.py
+++ ballkinsretry.py
@@ -1,8 +1,10 @@
 print("hello balkins")
-  for i in range(0,100):
-    if i%2==0:
+for i in range(0,100):
+  if i == 7:
     continue
+
   elif i%2!=0:
     print(i)
-  elif i=7:
-    pass;
+
+  else:
+    continue

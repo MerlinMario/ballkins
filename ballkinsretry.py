40e4205a49e1181bc473e5ea5f4e4b0343052069
MerlinMario <86819056+MerlinMario@users.noreply.github.com>
2021-08-20 13:18:12 +0530

    Update ballkinsretry.py

--- ballkinsretry.py
+++ ballkinsretry.py
@@ -1,8 +1,8 @@
 print("hello balkins")
-for i in range(0,100):
-if i%2==0:
-continue
-elif i%2!=0:
-print(i)
-elif i=7:
-pass;
+  for i in range(0,100):
+    if i%2==0:
+    continue
+  elif i%2!=0:
+    print(i)
+  elif i=7:
+    pass;
